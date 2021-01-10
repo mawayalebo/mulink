39074ded23038e547b32a3917b368366aa75424f
nerdfleeks <mawayalebo@users.noreply.github.com>
2021-01-09 18:54:36 -0800

    setting up app.js, adding home.ejs file, adding keys.js file

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose =require('mongoose');
+const { keys } = require('./keys');
+
+//set view engine
+app.set('view engine', 'ejs');
+
+//middleware to use assets in public folder
+app.use(express.static('public'));
+
+//connect to database
+mongoose.connect(keys.mongo.connectString,{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+    .then((results)=>{
+        app.listen(3000, ()=>{
+            console.log('light folding port 3000');
+        });
+    })
+    .catch(err=>console.log(err));
+
+//app routes
+
+app.get('/', (req, res)=>{
+    res.render('home');
+})
